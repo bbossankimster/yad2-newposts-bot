16f99e3c179440cb1cbd7de60616139dffb2264b
Andrey N <nepshekuev.a.88@gmail.com>
2024-01-08 00:28:41 +0300

    ref: rename columns

--- src/core/yad2_search_executor.py
+++ src/core/yad2_search_executor.py
@@ -143,7 +143,7 @@ class Yad2SearchNewPosts(Yad2Search):
         columns_df1 = [col for col in decreased_price_df.columns if '_df1' in col]
         decreased_normal_df = decreased_price_df[columns_df1]
         decreased_normal_df.columns = [col.replace('df1', '') for col in decreased_normal_df.columns]
-        decreased_normal_df[DATE_COL] = pd.to_datetime(decreased_normal_df[DATE_COL])
+        # decreased_normal_df[DATE_COL] = pd.to_datetime(decreased_normal_df[DATE_COL])
         return decreased_normal_df
 
     def _save_posts(self):
